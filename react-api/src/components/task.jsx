39161a7857ef5090f0b78a0140481bdae8e59acc
Lucavdb <92362088+Lucavdb@users.noreply.github.com>
2022-11-21 15:52:33 +0100

    task

--- react-api/src/components/task.jsx
+++ react-api/src/components/task.jsx
@@ -1,12 +1,34 @@
 import React from 'react';
 import '../styles/main.css';
 
-const Task = () => {
+function Task() {
     return (
         <div>
-
+                <div className="table-row">
+                    <div className="table-cell first-cell">
+                        <p>Tasks</p>
+                    </div>
+                    <div className="table-cell">
+                        <p>Assigne</p>
+                    </div>
+                    <div className="table-cell">
+                        <p>Status</p>
+                    </div>
+                    <div className="table-cell">
+                        <p>Priority</p>
+                    </div>
+                    <div className="table-cell">
+                        <p>Worked time</p>
+                    </div>
+                    <div className="table-cell">
+                        <p>Est time</p>
+                    </div>
+                    <div className="table-cell last-cell">
+                        <p>Due date</p>
+                    </div>
+                </div>
         </div>
     );
-};
+}
 
 export default Task;
--- react-api/src/pages/Home.jsx
+++ react-api/src/pages/Home.jsx
@@ -1,10 +1,15 @@
 import Header from "../components/header";
+import React from 'react';
+import Task from "../components/task";
+
+
 
 
 function Home() {
     return (
         <div>
             <Header />
+            <Task />
         </div>
     )
 }
