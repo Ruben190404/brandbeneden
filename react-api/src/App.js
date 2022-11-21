4a4577a34deeb88fea6e03bd4d7a71c247b43794
Sjors <sjors_thuis@hotmail.com>
2022-11-21 13:00:20 +0100

    nieuwe mappen structuur

--- react-api/src/App.js
+++ react-api/src/App.js
@@ -1,21 +1,19 @@
-import './styles/App.css';
-import logo from './Images/favicon.png';
+import Home from "./pages/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 
 function App() {
     return (
-        <div>
-            <header className="flex justify-between primary-background-colour h-20 drop-shadow">
-                <div className="nav-center-items w-[500px] pl-6">
-                    <a href="/"><img src={logo} className="w-16 h-16 border-4 border-purple-300 rounded-full shadow"/></a>
-                    <a href="/" className="purple-button w-48 h-12">Sprints</a>
-                    <a href="/" className="purple-button w-48 h-12">Burndown</a>
-                </div>
-                <div className="nav-center-items w-[300px] pr-6">
-                    <a href="/" className="purple-button w-44 h-12">Register</a>
-                    <img src="khjhgiu.png" className="center-items w-14 h-16 border-4 border-purple-300 rounded-xl shadow"/>
-                </div>
-            </header>
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route
+                    index
+                    element={
+                        <Home />
+                    }
+                />
+            </Routes>
+        </BrowserRouter>
     );
 }
 
---
+++ react-api/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import Header from "../components/header";
+
+
+function Home() {
+    return (
+        <div>
+            <Header />
+        </div>
+    )
+}
+
+export default Home
