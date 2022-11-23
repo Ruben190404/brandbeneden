6d0b2903a3c29441c7de36681fb833a60392f420
Lucavdb <92362088+Lucavdb@users.noreply.github.com>
2022-11-23 14:26:21 +0100

    task

--- react-api/src/components/header.jsx
+++ react-api/src/components/header.jsx
@@ -9,7 +9,7 @@ function Header() {
                 <div className="nav-center-items w-[500px] pl-6">
                     <a href="/"><img src={logo} className="w-16 h-16 border-4 border-purple-300 rounded-full shadow"/></a>
                     <a href="/" className="purple-button w-48 h-12">Sprints</a>
-                    <a href="/" className="purple-button w-48 h-12">Burndown</a>
+                    <a href="/pages/burndown" className="purple-button w-48 h-12">Burndown</a>
                 </div>
                 <div className="nav-center-items pr-6">
                     <img src={pfp} className="center-items w-14 h-16 border-4 border-purple-300 rounded-xl shadow"/>
