71cb4f7004a4711e0df69e44c8b0beb8e6b6a433
XD-Gamer-R <RLochem@outlook.com>
2022-11-23 10:33:49 +0100

    Sprint add & edit forms

--- react-api/src/pages/Home.jsx
+++ react-api/src/pages/Home.jsx
@@ -1,10 +1,21 @@
 import Header from "../components/header";
+import SprintForm from "../components/Sprint/create";
+import SprintShow from "../components/Sprint/show";
+import SprintEditForm from "../components/Sprint/edit";
 
 
 function Home() {
     return (
         <div>
             <Header />
+            <div className="flex flex-row">
+                <SprintShow why={"can't count"}/>
+                <SprintShow why={"sprint 69"}/>
+                <SprintShow why={"sprint 420"}/>
+            </div>
+
+            <SprintForm/>
+            <SprintEditForm/>
         </div>
     )
 }
