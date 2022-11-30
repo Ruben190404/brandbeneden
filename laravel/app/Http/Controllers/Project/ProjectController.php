338a75056247f737374709a0ba448af5b9489047
Ruben190404 <92361480+Ruben190404@users.noreply.github.com>
2022-11-30 01:06:09 +0100

    form boven op normale pagina en board styling fix

--- laravel/app/Http/Controllers/Project/ProjectController.php
+++ laravel/app/Http/Controllers/Project/ProjectController.php
@@ -21,9 +21,9 @@ class ProjectController extends Controller
     public function store(Request $request)
     {
         $validated = $request->validate([
-            'title' => 'required',
-            'start_date' => 'required',
-            'end_date' => 'required',
+            'title' => 'required | min:3',
+            'start_date' => 'required | date',
+            'end_date' => 'required | date | after:start_date',
         ]);
 
         $project = new Project();
