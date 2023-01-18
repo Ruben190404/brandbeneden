8521e3c8b4e5a1d7e62b4eee6f499135c58abb61
jcklerk <justin@klerk.nl>
2023-01-18 13:57:24 +0100

    cors fix ?

--- laravel-api/config/cors.php
+++ laravel-api/config/cors.php
@@ -15,11 +15,11 @@ return [
     |
     */
 
-    'paths' => ['api/*'],
+    'paths' => ['api/*', '*'],
 
-    'allowed_methods' => ['*'],
+    'allowed_methods' => ['POST', 'GET', 'DELETE', 'PUT', '*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['*', 'http://localhost:3000', 'https://laravel-api.test'],
 
     'allowed_origins_patterns' => [],
 
