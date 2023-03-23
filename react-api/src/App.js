ef56836c8905d28144a69e241091f672f64047f8
Ruben190404 <92361480+Ruben190404@users.noreply.github.com>
2023-03-23 13:08:24 +0100

    e

--- react-api/src/App.js
+++ react-api/src/App.js
@@ -7,25 +7,25 @@ import Burndown from "./pages/Burndown";
 import Dashboard from "./pages/Dashboard";
 import SprintEditForm from "./components/Sprint/edit";
 
-// import pop from "./sounds/pop.mp3";
-//
-// document.addEventListener('keydown', function(event) {
-//     // Create an audio element with the specified sound file
-//     var audioElement = document.createElement('audio');
-//     audioElement.setAttribute('src', pop);
-//
-//     // Play the sound
-//     audioElement.play();
-// });
-//
-// document.addEventListener('click', function(event) {
-//     // Create an audio element with the specified sound file
-//     var audioElement = document.createElement('audio');
-//     audioElement.setAttribute('src', pop);
-//
-//     // Play the sound
-//     audioElement.play();
-// })
+import pop from "./sounds/pop.mp3";
+
+document.addEventListener('keydown', function(event) {
+    // Create an audio element with the specified sound file
+    var audioElement = document.createElement('audio');
+    audioElement.setAttribute('src', pop);
+
+    // Play the sound
+    audioElement.play();
+});
+
+document.addEventListener('click', function(event) {
+    // Create an audio element with the specified sound file
+    var audioElement = document.createElement('audio');
+    audioElement.setAttribute('src', pop);
+
+    // Play the sound
+    audioElement.play();
+})
 
 function App() {
     const urlParams = new URLSearchParams(window.location.search);
